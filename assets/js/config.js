ef8e5423c03dc68df0c4be1c0e01cdc3ebb93b62
lazarluca2001 <lazarluca2001@gmail.com>
2026-03-11 15:15:10 +0100

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,18 @@
+export const CSV_URLS = {
+    PARTICIPANTS: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDDBNbIkZize7hPMfYPovbLgnIFWNuseLg0mjzDYGhLCwEEiF_-CiXnV76lgg2mvb54QabZ8y3Sork/pub?gid=689836805&single=true&output=csv",
+    RESULTS: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDDBNbIkZize7hPMfYPovbLgnIFWNuseLg0mjzDYGhLCwEEiF_-CiXnV76lgg2mvb54QabZ8y3Sork/pub?gid=1410406652&single=true&output=csv",
+    CALENDAR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDDBNbIkZize7hPMfYPovbLgnIFWNuseLg0mjzDYGhLCwEEiF_-CiXnV76lgg2mvb54QabZ8y3Sork/pub?gid=338581218&single=true&output=csv"
+};
+
+export const CAL_CONFIG = {
+    members: {
+        "Csongi": "🌈", "Merci": "🦆", "Mózes": "🦄", 
+        "Luca": "🐶", "Zoli": "🕺", "Viki": "🦦"
+    },
+    validStatuses: ["igen", "talán", "talan", "fizetve", "igazolt"],
+    months: ["JANUÁR", "FEBRUÁR", "MÁRCIUS", "ÁPRILIS", "MÁJUS", "JÚNIUS", "JÚLIUS", "AUGUSZTUS", "SZEPTEMBER", "OKTÓBER", "NOVEMBER", "DECEMBER"],
+    weekdays: ["H", "K", "Sze", "Cs", "P", "Szo", "V"]
+};
+
+export const ROLE_ORDER = ["leader", "follower"];
+export const DIVISION_ORDER = ["champion", "allstar", "advanced", "intermediate", "novice", "newcomer"];
