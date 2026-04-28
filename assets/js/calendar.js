0a64261f227142242060e16780f48711f71fcbbd
lazarluca2001 <lazarluca2001@gmail.com>
2026-04-28 15:53:04 +0200

    Add buildEventIndex call in calendar initialization

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -61,6 +61,7 @@ export async function initCalendarPage() {
         renderCalendar();
         renderFilters();
         renderStats();
+       buildEventIndex();
 
     } catch (e) {
         console.error("Calendar init error:", e);
